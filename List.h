b0584149019d50a3344335e381b69b7b803bd42e
Nahoa <rraymond001@gmail.com>
2017-01-10 13:32:32 -0800

    Update List.h
    1/10/17 
    Put node.h accidentally in List.h

--- List.h
+++ List.h
@@ -1,20 +1,27 @@
 #pragma once
+#include "Node.h"
 
-//class List<T>;
-
-
-template <typename T>
-class Node
+template<typename T>
+class List
 {
-	friend class List<T>;
+
 public:
-	Node();
-	Node & Node(const Node & copy);
-	Node & operator=(const Node & rhs);
-	~Node();
+	List();
+	List & List(const List & copy);
+	List & operator=(const List & rhs);
+	~List();
+	bool isEmpty();
+	Node & First() const;
+	Node & Last() const;
+	void Prepend(T Data); //adds to the front of the list
+	void Append(T Data); //adds to the end of the list maybe pointer?
+	void Purge();
+	void Extract(T Data);
+	void InsertAfter(T Data, T insertafter); //(element to insert, after other node)
+	void InsertBefore(T Data, T insertbefore); //(element to insert, before other node)
+	
 private:
-	T Data;
-	Node * m_Next;
-	Node * m_Previous;
+	Node<T> * m_Head;
+	Node<T> * m_Tail;
 };
 
