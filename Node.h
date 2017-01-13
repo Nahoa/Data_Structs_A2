36147b696812847619bf6fe93264dc84d1ee4fd6
Nahoa <rraymond001@gmail.com>
2017-01-13 01:03:50 -0800

    Update Node.h
    1/13/17
    updated header and added a new ctor

--- Node.h
+++ Node.h
@@ -1,6 +1,7 @@
 #pragma once
-
-//class List<T>;
+//forward declaration NEEDED
+template <typename T>
+class List;
 
 
 template <typename T>
@@ -9,32 +10,42 @@ class Node
 	friend class List<T>;
 public:
 	Node();
-	Node & Node(const Node & copy);
-	Node & operator=(const Node & rhs);
+	Node(T Data);
+	//Node * getnext() { return m_next; }//returns the next pointer
+	//Node * getprevious() { return m_previous; }// returns the previous pointer
+	/*Node & Node(const Node & copy);
+	Node & operator=(const Node & rhs);*/
 	~Node();
 private:
 	T Data;
-	Node * m_Next;
-	Node * m_Previous;
+	Node * m_next;
+	Node * m_previous;
 };
 
 template<typename T>
-inline Node<T>::Node()
+inline Node<T>::Node() : m_next(nullptr), m_previous(nullptr) //base memeber so the first node created points to null both ways
 {
 }
 
 template<typename T>
-inline Node<T>::Node(const Node & copy)
+inline Node<T>::Node(T Data) : Data(Data), m_next(nullptr),m_previous(nullptr) // creates a node with Data in it 
 {
+
 }
 
-template<typename T>
-inline Node & Node<T>::operator=(const Node & rhs)
-{
-	// TODO: insert return statement here
-}
+//template<typename T>
+//inline Node<T>::Node(const Node & copy)
+//{
+//}
+//
+//template<typename T>
+//inline Node & Node<T>::operator=(const Node & rhs)
+//{
+//	// TODO: insert return statement here
+//}
 
 template<typename T>
 inline Node<T>::~Node()
 {
+
 }
