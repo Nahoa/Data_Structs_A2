bd97313e110113cb06b3e8f2413b2ed643478ddc
Nahoa <rraymond001@gmail.com>
2017-01-13 01:02:23 -0800

    Update teststubb.cpp
    1/13/17
    Not done yet but some functions tested

--- teststubb.cpp
+++ teststubb.cpp
@@ -15,12 +15,18 @@ using std::cin;
 #include "Exception.h"
 
 bool test_default_ctor();
+bool test_is_Empty();
+bool test_copy_ctor();
+
+
 
 // Array of test functions
-FunctionPointer test_functions[] = { test_default_ctor };
+FunctionPointer test_functions[] = { test_default_ctor, test_is_Empty, test_copy_ctor, };
 
 int main()
 {
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+
 	bool tests_passed;
 
 	// Run the test functions
@@ -48,9 +54,87 @@ bool test_default_ctor()
 
 	 List<int> mylist;
 
-	// if(mylist.gethead() != nullptr)
+	 
+
+	// if (mylist.gethead() != nullptr && mylist.gettail() != nullptr)
+		// pass = false;
+
+	 //goes into function created an empty node 
 
 	cout << "Default Ctor Test ";
 
 	return pass;
 }
+
+
+//have to test purge and is empty 
+bool test_is_Empty() //Works!!!!!!!!!!!!!!!!!!!!!!!!!!
+{
+	bool pass = true;
+
+	List<int> mylist;
+
+	mylist.Prepend(5);
+	mylist.Prepend(6);
+	mylist.Prepend(7);
+
+	//mylist.Purge(); // purge works 
+
+	pass = mylist.isEmpty();
+
+	cout << "Is Empty Test ";
+
+	return pass;
+}
+
+bool test_copy_ctor()
+{
+	bool pass = true;
+	//int num_elements = 10;
+
+	List<int> mylist;
+
+	//for (int i = 0; i < num_elements; ++i)
+		//val_len[i] = test_data[i]; // have to append the data
+
+	List<int> mylistcpy = mylist;
+
+	// Check data integrity
+	//for (int i = 0; i < num_elements && pass; ++i)
+	//{
+		//if (mylistcpy.search() != mylist)
+			//pass = false;
+	//}
+
+	cout << "Copy ctor test ";
+
+	return pass;
+}
+
+//bool test_op_eql()
+//{
+//	bool pass = true;
+//
+//	int num_elements = 10;  // Change this if test data changes
+//	int test_data[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
+//
+//	Array<int> val_len(10);
+//
+//	for (int i = 0; i < num_elements; ++i)
+//		val_len[i] = test_data[i];
+//
+//	Array<int> val_len_cpy;
+//
+//	val_len_cpy = val_len;
+//
+//	// Check data integrity
+//	for (int i = 0; i < num_elements && pass; ++i)
+//	{
+//		if (val_len_cpy[i] != test_data[i])
+//			pass = false;
+//	}
+//
+//	cout << "Op equal test ";
+//
+//	return pass;
+//}
