2b74dc1cc058cc82130a70409481ff882f9f29f9
Nahoa <rraymond001@gmail.com>
2017-01-14 01:09:25 -0800

    Update Node.h
    1/14/17
    Finished up and tested functions

--- Node.h
+++ Node.h
@@ -11,10 +11,8 @@ class Node
 public:
 	Node();
 	Node(T Data);
-	//Node * getnext() { return m_next; }//returns the next pointer
-	//Node * getprevious() { return m_previous; }// returns the previous pointer
-	/*Node & Node(const Node & copy);
-	Node & operator=(const Node & rhs);*/
+	Node(const Node & copy);
+	Node & operator=(const Node & rhs);
 	~Node();
 private:
 	T Data;
@@ -33,16 +31,20 @@ inline Node<T>::Node(T Data) : Data(Data), m_next(nullptr),m_previous(nullptr) /
 
 }
 
-//template<typename T>
-//inline Node<T>::Node(const Node & copy)
-//{
-//}
-//
-//template<typename T>
-//inline Node & Node<T>::operator=(const Node & rhs)
-//{
-//	// TODO: insert return statement here
-//}
+template<typename T>
+inline Node<T>::Node(const Node & copy) :m_Data(copy.m_Data), m_Next(copy.m_Next), m_Previous(copy.m_Previous)
+{
+}
+
+template<typename T>
+inline Node<T> & Node<T>::operator=(const Node & rhs)
+{
+	if (this != &rhs)
+	{
+		m_Data = rhs.m_Data;
+	}
+	return *this;
+}
 
 template<typename T>
 inline Node<T>::~Node()
